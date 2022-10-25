587022ae181fb0d023d7e604b8e04cc855cab207
Vinay4007 <cs20b049@iittp.ac.in>
2022-10-25 10:31:21 +0530

    project struct modified

--- Back/db/connec1.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-const DB1 = process.env.MONG_URI1;
-mongoose.connect(DB1).then(() => {
-    console.log('connection1 successful');
-  }).catch((err) => console.log('no connection'));
